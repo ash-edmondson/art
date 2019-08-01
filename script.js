6b667c6f17ac0986bc5ffa54c3a9e27de7021ab5
Ash Edmondson <ash@ashedmondson.me.uk>
2019-08-01 17:36:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+var art= document.getElementById('art')
+var c = art.getContext('2d')
+
+c.beginPath()
+
+//     x   y   w   h
+//Blue
+c.rect(0, 0, 400, 75)
+c.fillStyle= "#55cdfc"
+c.fill()
+
+c.beginPath()
+//     x   y   w   h
+//pink
+c.rect(0, 75, 400, 75)
+c.fillStyle= "#f7a8b8"
+c.fill()
+
+c.beginPath()
+//     x   y   w   h
+//White
+c.rect(0, 150, 400, 75)
+c.fillStyle= "#ffffff"
+c.fill()
+
+c.beginPath()
+//     x   y   w   h
+//pink
+c.rect(0, 225, 400, 75)
+c.fillStyle= "#f7a8b8"
+c.fill()
+
+c.beginPath()
+
+//     x   y   w   h
+//Blue
+c.rect(0, 300, 400, 75)
+c.fillStyle= "#55cdfc"
+c.fill()
+//c.beginPath()
+//    x   y   r   st  end
+//c.arc(50, 50, 10, 0 , 2*Math.PI)
+//c.strokeStyle = "Red"
+//c.stroke()
+
+//c.beginPath()
+//c.moveTo (100, 100)
+//c.lineTo(130,100)
+//c.lineTo(130,130)
+//c.lineTo(100,100)
+//c.fill()
